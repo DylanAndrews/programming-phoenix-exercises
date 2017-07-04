dd2e50bfeb4eb0bbf54da6a4949b2a66a3f94ab7
Dylan Andrews <dylanandr@gmail.com>
2017-07-04 18:52:31 -0500

    Get create, show, index, and new routes set up with real db

--- rumbl/web/views/user_view.ex
+++ rumbl/web/views/user_view.ex
@@ -1,8 +1,11 @@
+require IEx
+
 defmodule Rumbl.UserView do
   use Rumbl.Web, :view
   alias Rumbl.User
 
   def first_name(%User{name: name}) do
+    # IEx.pry
     name
     |> String.split(" ")
     |> Enum.at(0)
